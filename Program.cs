3cf9e61dc3cbaabd153b6315c3f0cdeb3b43585b
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-04-06 11:43:14 -0300

    Implementação da classe Indivíduo

--- Program.cs
+++ Program.cs
@@ -15,43 +15,12 @@ namespace CalculadoraIMC
             Console.WriteLine("Entre com o seu peso em Kg. Ex: 67,7");
             float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = peso / (altura * altura);
+            Individuo i1 = new Individuo(nome, peso, altura);
 
-            Console.WriteLine("{0}, sua altura é {1} e seu peso {2}", nome, altura, peso);
+            Console.WriteLine(i1.IMC);
+            Console.WriteLine(i1.ClassificacaoIMC());
+            Console.WriteLine(i1.ParaPesoIdeal());
 
-            Console.WriteLine("Seu IMC é {0}", imc);
-
-            double pesoNormalMinimo = altura * altura * 18.5;
-            double pesoNormalMaximo = altura * altura * 25;
-            double pesoFaltante = pesoNormalMinimo - peso;
-            double pesoExcesso = peso - pesoNormalMaximo;
-            if(imc < 17 ){
-                Console.WriteLine("Está muito abaixo do peso. Deveria receber mais {0} Kg", pesoFaltante);
-            }
-
-            else if(imc >= 17 && imc < 18.5 ){
-                Console.WriteLine("Está abaixo do peso. Deveria receber mais {0} Kg", pesoFaltante);
-            }
-
-            else if(imc >= 18.5 && imc < 25){
-                 Console.WriteLine("Está com peso normal");
-            }
-
-            else if(imc >= 25 && imc < 30){
-                 Console.WriteLine("Está acima do peso. Deveria perder {0} Kg", pesoExcesso);
-            }
-
-            else if(imc >= 30 && imc < 35){
-                 Console.WriteLine("Está com obsidade I. Deveria perder {0} Kg", pesoExcesso);
-            }
-
-            else if(imc >= 35 && imc < 40){
-                 Console.WriteLine("Está com obsidade II. Deveria perder {0} Kg", pesoExcesso);
-            }
-
-            else {
-                 Console.WriteLine("Está com obsidade III. Deveria perder {0} Kg", pesoExcesso);
-            }
         }
     }
 }
